18dc304d9cdc4ae74f5ccbc716595ce74c5593fc
Suyeong-Lim <tntn393@gmail.com>
2022-08-17 20:00:11 +0900

    fix: Image config 수정

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  reactStrictMode: true,
-};
-
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["wine21.speedgabia.com"],
   },
 };
-
-module.exports = nextConfig;
