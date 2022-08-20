c82debcfc52c2c4e80490104c9ea0b58de75bdd2
Suyeong-Lim <tntn393@gmail.com>
2022-08-20 18:16:55 +0900

    add: winePropertyCard 컴포넌트 추가

---
+++ src/containers/searching/components/PropertyItem.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import Text from "components/common/Text";
+import Spacing from "components/common/Spacing";
+
+const PropertyItem = ({ label, status }) => {
+  return (
+    <StyledPropertyItem>
+      <Text size="xs" color="descText" weight="light">
+        {label}
+      </Text>
+      <Spacing height={9} />
+      <Text size="xs" color="gray900" weight="regular">
+        <span>{status ?? "-"}</span>
+      </Text>
+    </StyledPropertyItem>
+  );
+};
+
+export default PropertyItem;
+
+const StyledPropertyItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 2rem;
+`;
---
+++ src/containers/searching/components/WinePropertyCard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import PropertyItem from "containers/searching/components/PropertyItem";
+
+const LABEL_BY_LEVEL: Record<number, string> = {
+  [1]: "매우낮음",
+  [2]: "낮음",
+  [3]: "보통",
+  [4]: "높음",
+  [5]: "매우높음",
+};
+
+const BODY_LABEL_BY_LEVEL: Record<number, string> = {
+  [1]: "매우가벼움",
+  [2]: "가벼움",
+  [3]: "보통",
+  [4]: "무거움",
+  [5]: "매우무거움",
+};
+
+const WinePropertyCard = ({ wineDetail }) => {
+  const [wineDetailItem] = [...wineDetail];
+  const {
+    name,
+    enName,
+    rate,
+    image,
+    country,
+    type,
+    tannins,
+    body,
+    sweetness,
+    acidity,
+  } = wineDetailItem;
+
+  return (
+    <Container>
+      <PropertyItem label="탄닌" status={tannins && LABEL_BY_LEVEL[tannins]} />
+      <PropertyItem label="바디감" status={body && BODY_LABEL_BY_LEVEL[body]} />
+      <PropertyItem label="당도" status={body && LABEL_BY_LEVEL[sweetness]} />
+      <PropertyItem label="산도" status={body && LABEL_BY_LEVEL[acidity]} />
+    </Container>
+  );
+};
+
+export const Container = styled.div`
+  display: flex;
+  border-radius: 12px;
+  background: ${({ theme }) => theme.colors.brighterBg};
+  padding: 8px 20px;
+`;
+
+export default WinePropertyCard;
