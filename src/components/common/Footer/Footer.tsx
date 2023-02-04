a043c6d633befceb670b4844f80a9c6e75d100e7
201411108 <aggsae@gmail.com>
2023-02-05 01:00:50 +0900

    refactor: 별도 styles 파일 제거

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,36 +1,37 @@
 import React from "react";
 import Image from "next/image";
-import * as S from "./style";
 
 import IcoTistory from "../../../../public/images/tistoryW.svg";
 import faceBookw from "../../../../public/images/faceBookW.svg";
 import instaW from "../../../../public/images/instaW.svg";
 import Link from "next/link";
+import styled from "styled-components";
+import media from "@/styles/media";
 
 const Footer = () => {
   return (
-    <S.Wrapper>
-      <S.InfoContainer>
-        <S.Logo>
+    <Wrapper>
+      <InfoContainer>
+        <Logo>
           <Image
             src="/images/Minery.png"
             alt="minery"
             layout="fill"
             objectFit="cover"
           />
-        </S.Logo>
-        <S.FooterDesc>
-          <S.Contact>
+        </Logo>
+        <FooterDesc>
+          <Contact>
             <p>CONTACT</p>
             <p> | </p>
             <p>minery.app@gmail.com</p>
-          </S.Contact>
+          </Contact>
           <Link href="https://han.gl/oUmxW">
-            <S.Info>개인정보처리방침</S.Info>
+            <Info>개인정보처리방침</Info>
           </Link>
-        </S.FooterDesc>
+        </FooterDesc>
 
-        <S.SNSContainer>
+        <SNSContainer>
           <Link href="https://www.instagram.com/minery_app/?igshid=YmMyMTA2M2Y%3D">
             <a>
               <Image
@@ -64,10 +65,108 @@ const Footer = () => {
               />
             </a>
           </Link>
-        </S.SNSContainer>
-      </S.InfoContainer>
-    </S.Wrapper>
+        </SNSContainer>
+      </InfoContainer>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.footer`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 285px;
+  background-color: ${({ theme }) => theme.colors.gray900};
+
+  ${media.tablet} {
+    height: 210px;
+  }
+
+  ${media.tabletS} {
+    flex-direction: column;
+    height: 290px;
+  }
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1200px;
+
+  ${media.tablet} {
+    width: 800px;
+  }
+
+  ${media.tabletS} {
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: flex-start;
+    width: 390px;
+    height: 100%;
+  }
+`;
+
+const Logo = styled.div`
+  position: relative;
+  align-self: flex-start;
+  width: 98px;
+  height: 27px;
+
+  ${media.tabletS} {
+    width: 72.57px;
+    height: 20px;
+  }
+`;
+
+const FooterDesc = styled.span`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const Contact = styled.span`
+  display: flex;
+  color: ${({ theme }) => theme.colors.white};
+  p {
+    font-size: 13px;
+    margin-right: 12px;
+  }
+
+  ${media.tablet} {
+    p {
+      font-size: 12px;
+    }
+  }
+`;
+
+const Info = styled.a`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 13px;
+  font-weight: 700;
+  margin-top: 20px;
+
+  ${media.tablet} {
+    font-size: 12px;
+  }
+`;
+
+const SNSContainer = styled.div`
+  display: flex;
+  a {
+    position: relative;
+    display: block;
+    width: 40px;
+    height: 40px;
+    margin-right: 30px;
+
+    ${media.tablet} {
+      width: 26px;
+      height: 26px;
+    }
+  }
+`;
 
 export default Footer;
