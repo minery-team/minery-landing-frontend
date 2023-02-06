f112503e8b72509ba6c278708c456c0f4f45b3be
201411108 <aggsae@gmail.com>
2023-02-06 22:57:36 +0900

    fix: import 구문 누락으로 인한 빌드 에러 수정

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import styled from "styled-components";
 import media from "@/styles/media";
+import SNSMenu from "../SNSMenu";
 
 const Footer = () => {
   return (
