097c3c57bde10ed12fc84cf4091ec4ee19db3807
HC-Gwon <gurckddlz@naver.com>
2023-02-16 00:51:21 +0900

    refactor: FirstSection 디자인 수정에 맞춰 코드 수정

--- src/containers/landing/components/section/FirstSection.tsx
+++ src/containers/landing/components/section/FirstSection.tsx
@@ -6,34 +6,40 @@ import Card from "@/components/common/Card";
 import Button from "@/components/common/Button";
 import Spacing from "@/components/common/Spacing";
 import { Title } from "@/components/common/Title";
+import useWindowWidth from "@/hooks/useWindowWidth";
+
 const FirstSection = () => {
   const { reviewData } = FIRST_CONTENTS;
+  const width = useWindowWidth();
   const instaLink =
     "https://www.instagram.com/minery_app/?igshid=YmMyMTA2M2Y%3D";
+
+  const getButtonSize = () => {
+    if (width < 1199) return { width: 190, height: 48 };
+    return { width: 196, height: 60 };
+  }
+  
   return (
     <SectionWrapper>
       <DescWrapper>
-        <StyledTitle text="mainText" direction="row">
-          <p>와인으로 쓰는</p>
-          <p>그 날의 일기</p>
+        <StyledTitle text="mainText">
+          {width > 767 ? <div>와인으로 쓰는 그 날의 일기</div> : <><div>와인으로 쓰는</div><div>그 날의 일기</div></>}
         </StyledTitle>
-        <Spacing height={2} />
+        <Spacing height={16} />
         <Title text="subText">
-          <p>와인이 있어 더 특별했던 하루 !</p>
-          <p>내 마음대로 기록하고 공유해 보세요</p>
+          {'와인이 있어 더 특별했던 하루 !\n내 마음대로 기록하고 공유해 보세요'}
         </Title>
-        <Spacing height={2} />
         <CardList>
           {reviewData.map((data, index) => {
             return <Card key={index} data={data} />;
           })}
         </CardList>
       </DescWrapper>
-      <Spacing height={2} />
+      <Spacing height={60} />
       <Button
-        width={150}
-        height={40}
-        borderRadius={28}
+        width={getButtonSize().width}
+        height={getButtonSize().height}
+        borderRadius={71.5}
         onClick={() => window.open(instaLink)}
       >
         와인일기 구경하기
@@ -69,12 +75,26 @@ const DescWrapper = styled.div`
 
 const CardList = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: no-wrap;
   justify-content: center;
   align-items: center;
   overflow: hidden;
+
   ${media.mobile} {
     justify-content: space-evenly;
+    flex-wrap: wrap;
+  }
+
+  ${media.tablet} {
+    div:nth-child(1) {
+      display: none;
+    }
+    div:nth-child(4) {
+      display: none;
+    }
+    div:nth-child(5) {
+      display: none;
+    }
   }
 `;
 
