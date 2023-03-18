8ce1ef42c26a4a4be5b8fa850f3035bb8b76bf9e
HC-Gwon <gurckddlz@naver.com>
2023-03-18 17:17:36 +0900

    fix: 공유받은 일기를 볼 땐 웹 Header 안보이도록 수정

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,19 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React, { PropsWithChildren } from "react";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 
 interface Props extends PropsWithChildren {}
 
 const Layout = ({ children }: Props) => {
+  const router = useRouter();
+  const isDiaryPage = router.pathname.includes('diary');
+
   return (
     <Wrapper>
-      <Header />
-      <Container>{children}</Container>
+      {!isDiaryPage && <Header />}
+      <Container isDiaryPage={isDiaryPage}>{children}</Container>
       <Footer />
     </Wrapper>
   );
@@ -22,7 +26,7 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 
-const Container = styled.main`
+const Container = styled.main<{ isDiaryPage: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -31,7 +35,7 @@ const Container = styled.main`
   z-index: 1;
   overflow: hidden;
   flex: 1;
-  padding-top: 65px;
+  padding-top: ${({ isDiaryPage }) => !isDiaryPage && '65px'};
 `;
 
 export default Layout;
